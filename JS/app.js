fa64da8b35f1293491a104b550ce1f545e71d037
Alyssa Champion <alyssa.champion2016@gmail.com>
2021-03-26 23:47:25 -0700

    added js code to app.js

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,21 @@
 // your JavaScript goes here, you can delete this comment
+const completed_todos = [];
+const todo_list = [
+	{todo: "clone repo for starter code", status: "complete"},
+  {todo: "write code for html", status: "started"},
+  {todo: "add colour to css", status: "complete"},
+  {todo: "make good git commits", stauts: "started"}
+  
+];
+
+for(const todo in todo_list){
+	if (todo_list[todo].status == "complete") {
+  	completed_todos.push(todo_list[todo])
+  }
+}
+
+console.log(completed_todos)
+
+for (let {todo} of completed_todos){
+	console.log(todo)
+}
